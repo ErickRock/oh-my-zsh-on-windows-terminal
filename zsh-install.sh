2378e869c847304a606d128bd61544a6b3245b20
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2020-12-09 21:00:53 -0300

    Exit 0 para Exit 3

--- zsh-install.sh
+++ zsh-install.sh
@@ -39,7 +39,7 @@ done
 if command -v zsh
 then
     # Se sim, altera a shell padrão do usuário para o ZSH
-    sudo chsh -s "$(command -v zsh)" "\033[1;33m$USER\033[0m"
+    sudo chsh -s "$(command -v zsh)" "$USER"
 else
     # Senão, exibe mensagem de erro e sai
     echo "\033[0;32m Não foi encontrado o executável do ZSH no sistema, saindo com erro...\033[0m"
@@ -49,4 +49,4 @@ fi
 echo "\033[0;32m Instalação do Git e ZSH concluída com Êxito!!\033[0m"
 echo "\033[0;32m Feche o terminal e abra novamente!!\033[0m"
 
-exit 0
+exit 3
