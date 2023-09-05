d1686125cc53531d9534935b7e1288be59b16cf1
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2023-09-05 17:34:59 -0300

    fix bugs

--- zsh-install.sh
+++ zsh-install.sh
@@ -5,17 +5,25 @@
 
 cd ~ || exit
 
-for comando in "apt-get update -q" "apt-get upgrade -yq"
-do
-    echo "Executando comando: $comando..."
-...(17 lines omitted)...
-        if sudo apt-get install -f
-        then
-            echo "Pacotes quebrados recuperados com sucesso."
-        else
-            echo "Falha ao recuperar pacotes quebrados."
-        fi
-    fi
-done
+# Instalação do Git
+echo "Iniciando instalação do Git..."
+sudo apt-get install -y git
 
-echo "Instalação do Git e ZSH concluída com êxito!"
+# Verifica se o Git já está instalado
+if command -v git >/dev/null 2>&1; then
+    echo "Git já está instalado"
+else
+    echo "Falha ao instalar o Git"
+    exit 1
+fi
+
+# Verifica se o ZSH já está instalado
+if command -v zsh >/dev/null 2>&1; then
+    echo "ZSH já está instalado"
+else
+    # Instalação do ZSH
+    echo "Iniciando instalação do ZSH..."
+    sudo apt-get install -y zsh
+fi
+
+echo "Instalação do Git e ZSH concluída com sucesso!"
