4dfb6812a7284108f3fcacd56b5fd809da91ba76
ErickRock <erickgarciagodoy@outlook.com>
2023-09-10 04:57:08 -0300

    feat: adiciona cores e verifica instalação (#12)

--- zsh-install.sh
+++ zsh-install.sh
@@ -6,24 +6,24 @@
 cd ~ || exit
 
 # Instalação do Git
-echo "Iniciando instalação do Git..."
+echo -e "\e[33mIniciando instalação do Git...\e[0m"
 sudo apt-get install -y git
 
 # Verifica se o Git já está instalado
 if command -v git >/dev/null 2>&1; then
-    echo "Git já está instalado"
+    echo -e "\e[32mGit já está instalado\e[0m"
 else
-    echo "Falha ao instalar o Git"
+    echo -e "\e[31mFalha ao instalar o Git\e[0m"
     exit 1
 fi
 
 # Verifica se o ZSH já está instalado
 if command -v zsh >/dev/null 2>&1; then
-    echo "ZSH já está instalado"
+    echo -e "\e[32mZSH já está instalado\e[0m"
 else
     # Instalação do ZSH
-    echo "Iniciando instalação do ZSH..."
+    echo -e "\e[33mIniciando instalação do ZSH...\e[0m"
     sudo apt-get install -y zsh
 fi
 
-echo "Instalação do Git e ZSH concluída com sucesso!"
+echo -e "\e[32mInstalação do Git e ZSH concluída com sucesso!\e[0m"
